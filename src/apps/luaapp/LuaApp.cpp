dc70e6c10823d0e75ce5d19f64dd55b32c30102a
Mirkokral <mirkogodak@gmail.com>
2024-02-11 20:06:32 +0100

    luaapp finalize interface

--- src/apps/luaapp/LuaApp.cpp
+++ src/apps/luaapp/LuaApp.cpp
@@ -1,8 +1,15 @@
 #include "LuaApp.hpp"
 bool luaappopen = true;
+lv_obj_t* table;
+lv_obj_t* input;
+
+#define append(item) lv_table_set_cell_value(table, lv_table_get_column_count(table), 0, item); lv_table_set_column_width(table, lv_table_get_column_count(table) - 1, 320)
+
 lv_obj_t *LuaApp::prepareScreen() {
     lv_obj_t* scr = lv_obj_create(NULL);
-    lv_obj_t* consolegroup = lv_obj_create(scr);
+    table = lv_table_create(scr);
+    input = lv_textarea_create(scr);
+    // lv_obj_t* consolegroup = lv_obj_create(scr);
     lv_obj_t* closebutton = lv_button_create(scr);
     lv_obj_t* closebuttonlbl = lv_label_create(closebutton);
     lv_obj_align(closebutton, LV_ALIGN_TOP_RIGHT, 0, 0);
@@ -10,15 +17,14 @@ lv_obj_t *LuaApp::prepareScreen() {
         luaappopen = false;
     }, LV_EVENT_CLICKED, NULL);
     lv_label_set_text(closebuttonlbl, LV_SYMBOL_CLOSE);
-    lv_obj_set_size(consolegroup, 320, 240);
-    lv_obj_set_style_radius(consolegroup, 0, LV_PART_MAIN);
-    lv_obj_set_style_border_width(consolegroup, 0, LV_PART_MAIN);
-    lv_obj_set_flex_align(consolegroup, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
-    lv_obj_set_flex_flow(consolegroup, LV_FLEX_FLOW_COLUMN);
-    lv_label_set_text(lv_label_create(consolegroup), "Test 1");
-    lv_label_set_text(lv_label_create(consolegroup), "Test 2");
-    lv_label_set_text(lv_label_create(consolegroup), "Test 3");
-
+    lv_obj_align(table, LV_ALIGN_TOP_MID, 0, 0);
+    lv_obj_align(input, LV_ALIGN_BOTTOM_MID, 0, 0);
+    lv_textarea_set_one_line(input, true);
+    lv_obj_set_size(input, 320, 40);
+    lv_obj_set_size(table, 320, 240 - 40);
+    lv_table_set_column_count(table, 1);
+    lv_table_set_cell_value(table, 0, 0, "Lua interpreter");
+    append("test");
     return scr;
 }
 
@@ -26,5 +32,7 @@ void LuaApp::taskFunction( ){
     while(luaappopen) {
         delay(100);
     }
+    lv_obj_delete(table);
+    lv_obj_delete(input);
     luaappopen=true;
 }
